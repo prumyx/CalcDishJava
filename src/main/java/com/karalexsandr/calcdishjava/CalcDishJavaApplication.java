a7f53fb95e53704a3e9f52fee7de086fa0ab23de
prumix <prumixxx@gmail.com>
2022-08-04 16:20:01 +0200

    Initial commit

---
+++ src/main/java/com/karalexsandr/calcdishjava/CalcDishJavaApplication.java
@@ -0,0 +1,13 @@
+package com.karalexsandr.calcdishjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcDishJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalcDishJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/karalexsandr/calcdishjava/CalcDishJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karalexsandr.calcdishjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcDishJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
